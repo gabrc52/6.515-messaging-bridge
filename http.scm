c3ff3d62d82a6f37b1115aafe94175ac51d3513f
Gabriel Rodríguez <rgabriel@mit.edu>
2025-04-19 22:29:05 -0400

    Update http.scm

--- http.scm
+++ http.scm
@@ -45,3 +45,56 @@
 	(newline)
 	(loop (+ i 1)))))
 
+(string->jsexpr (curl-http-request "GET" "https://api.github.com" "/users/vi"))
+
+;; Back to using the builtin HTTP implementation
+;; HTTP suffices thanks to stunnel
+
+(define (http-get-string domain path #!optional headers)
+  (let ((headers (if (default-object? headers) (list) headers))
+	(url (string-concatenate (list "http://" domain path))))
+    (utf8->string (http-response-body (http-get url headers)))))
+
+(define (http-get-json domain path #!optional headers)
+  (string->jsexpr (http-get-string domain path headers)))
+
+;; If we wanted to, we can easily implement http-put copying the implementation of http-post but
+;; changing it to PUT
+(define (http-put uri headers body)
+  (http-client-exchange "PUT" (->uri uri) headers body))
+
+(http-get-json "127.0.0.1:20001" "/advice")
+
+(http-get "http://127.0.0.1:20001/advice" '())
+
+(define response (http-get "http://hydrant.mit.edu/latestTerm.json" '()))
+(string->jsexpr (utf8->string (http-response-body response)))
+
+;; Example Matrix unauthenticated endpoint (gets the types of logins that a server supports)
+"/_matrix/client/v3/login"
+
+(http-get "http://127.0.0.1:10002/_matrix/client/v3/login" '())
+(string->jsexpr (curl-http-request "GET" "http://127.0.0.1:10002" "/_matrix/client/v3/login"))
+;The object #[binary-output-port 13], passed as an argument to write, is not the correct type.
+;To continue, call RESTART with an option number:
+; (RESTART 1) => Return to read-eval-print level 1.
+;Start debugger? (y or n):
+
+;; This works
+(string->jsexpr (curl-http-request "GET" "https://mattermost.mit.edu/" "api/v4/config/client?format=old"))
+
+;; This also works
+(string->jsexpr (curl-http-request "GET" "http://127.0.0.1:10001/" "api/v4/config/client?format=old"))
+
+
+(http-get "http://127.0.0.1:10001/api/v4/config/client?format=old" '())
+
+;; This connects to the ChatGPT-generated proxy which does not actually work with query strings?
+;; But it does not work either
+(http-get "http://127.0.0.1:8080/api/v4/config/client?format=old" '())
+
+;; Does it work with ANYTHING localhost then???
+
+(http-get "http://localhost:10001/api/v4/config/client?format=old" '())
+
+;; None of the http-get work
