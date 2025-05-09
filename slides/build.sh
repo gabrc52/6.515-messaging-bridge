dc16311a733b1c41b7fae1eb3cf73414b6f3c3b1
Gabriel Rodríguez <rgabriel@mit.edu>
2025-05-08 21:20:50 -0400

    Remove sandbox and slides

--- slides/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# There are two options:
-pandoc -t revealjs -s -o index.html slides.md -V revealjs-url=https://unpkg.com/reveal.js -V theme=sky --slide-level=2
-pandoc -t beamer slides.md -V theme:Warsaw -o slides.pdf
